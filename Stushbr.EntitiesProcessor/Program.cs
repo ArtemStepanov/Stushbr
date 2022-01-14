e3d4bc97bce48ab01a114d3f81efe952cf7afbb1
Artem Stepanov <stxima@gmail.com>
2022-01-14 18:08:15 +0300

    injection

--- Stushbr.EntitiesProcessor/Program.cs
+++ Stushbr.EntitiesProcessor/Program.cs
@@ -1,7 +1,7 @@
 using Stushbr.EntitiesProcessor;
 using Stushbr.EntitiesProcessor.Configuration;
+using Stushbr.EntitiesProcessor.Processors;
 using Stushbr.EntitiesProcessor.Services;
-using Stushbr.Shared.Configuration;
 using Stushbr.Shared.Extensions;
 using Telegram.Bot;
 
@@ -10,14 +10,29 @@ IHost host = Host.CreateDefaultBuilder(args)
     {
         var config = context.Configuration.Get<ProcessorConfiguration>();
         services.AddEssentials(config);
+
+        #region Processors
+
+        services.AddSingleton<ITelegramChannelProcessor, TelegramChannelProcessor>();
+
+        #endregion
+
+        #region Services
+
         services.AddSingleton<IItemProcessorService, ItemProcessorService>();
-        services.AddSingleton<ITelegramBotService, TelegramBotService>();
         services.AddSingleton<ITelegramBotService>(provider => new TelegramBotService(
             provider.GetRequiredService<ILogger<TelegramBotService>>(),
             new TelegramBotClient(config.Telegram.AccessToken)
         ));
+
+        #endregion
+
+        #region Hosted services
+
         services.AddHostedService<BillStatusUpdater>();
         services.AddHostedService<ItemProcessorWorker>();
+
+        #endregion
     })
     .Build();
 
