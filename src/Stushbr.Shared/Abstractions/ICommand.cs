61c0f47eca408363b9e033ce6af6dc4628d2d1a3
Artem Stepanov <stxima@gmail.com>
2023-09-21 11:42:55 +0300

    cleanup

---
+++ src/Stushbr.Shared/Abstractions/ICommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Stushbr.Shared.Abstractions;
+
+public interface ICommand : IRequest
+{
+}
+
+public interface ICommand<out TResult> : IRequest<TResult>
+{
+}
---
+++ src/Stushbr.Shared/Commands/TestCommand.cs
@@ -0,0 +1,4 @@
+﻿using Stushbr.Shared.Abstractions;
+
+namespace Stushbr.Shared.Commands;
+public sealed record TestCommand(string Blah) : ICommand<string>;
