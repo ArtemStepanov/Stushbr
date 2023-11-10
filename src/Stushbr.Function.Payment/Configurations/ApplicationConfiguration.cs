b7008ad0f428a1319117f9a502ba3c99e8e409e3
Artem Stepanov <stxima@gmail.com>
2023-11-10 02:26:44 +0200

    Add Tilda webhook secret to payment hook
    Added a 'TildaWebhookSecret' property to 'ApplicationConfiguration' to validate the Access Token from Tilda's webhook requests. Updated the PaymentHook to check this token in the headers of incoming requests. If the token doesn't match or is missing, the request will be unauthorized. This is to ensure that only authorized sources can trigger payment hooks.

--- src/Stushbr.Function.Payment/Configurations/ApplicationConfiguration.cs
+++ src/Stushbr.Function.Payment/Configurations/ApplicationConfiguration.cs
@@ -3,4 +3,6 @@ namespace Stushbr.Function.Payment.Configurations;
 public sealed class ApplicationConfiguration
 {
     public bool MigrationMode { get; set; }
+
+    public string TildaWebhookSecret { get; set; } = default!;
 }
