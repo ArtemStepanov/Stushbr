6df4bc5656aa04c59ab3d8ce0eeb398f3dcbe61e
Artem Stepanov <stxima@gmail.com>
2023-11-11 05:55:32 +0200

    Replace react app with Razor MVC application

---
+++ src/admin-util/Stushbr.AdminUtilsWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Stushbr.AdminUtilsWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
